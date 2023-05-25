25a36f19258d1211ed1c8b74ae11abc4064fab0d
Jasper <japsu.honkasalo@gmail.com>
2023-05-25 15:22:03 +0300

    Add missing meta files, remove old Logger declarations

--- Eflatun.SceneReference/Packages/com.eflatun.scenereference/Runtime/Logger.cs
+++ Eflatun.SceneReference/Packages/com.eflatun.scenereference/Runtime/Logger.cs
@@ -2,9 +2,9 @@
 {
     internal static class Logger
     {
-        internal static void Debug(string msg) => UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
-        internal static void Warn(string msg) => UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
-        internal static void Error(string msg) => UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
+        //internal static void Debug(string msg) => UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
+        //internal static void Warn(string msg) => UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
+        //internal static void Error(string msg) => UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
         
         
         internal static void Debug(string msg)
