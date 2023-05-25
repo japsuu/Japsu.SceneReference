d46805f740442faa64734e518c7177a2d611915a
Jasper <japsu.honkasalo@gmail.com>
2023-05-25 16:04:28 +0300

    Split Logger to editor and runtime loggers

--- Runtime/Logger.cs
+++ Runtime/Logger.cs
@@ -2,29 +2,8 @@
 {
     internal static class Logger
     {
-        //internal static void Debug(string msg) => UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
-        //internal static void Warn(string msg) => UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
-        //internal static void Error(string msg) => UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
-        
-        
-        internal static void Debug(string msg)
-        {
-            if(Eflatun.SceneReference.Editor.SettingsManager.SceneGuidToPathMap.EnableInfoLogging.value)
-                UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
-        }
-        
-        
-        internal static void Warn(string msg)
-        {
-            if(Eflatun.SceneReference.Editor.SettingsManager.SceneGuidToPathMap.EnableWarnLogging.value)
-                UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
-        }
-        
-        
-        internal static void Error(string msg)
-        {
-            if(Eflatun.SceneReference.Editor.SettingsManager.SceneGuidToPathMap.EnableErrorLogging.value)
-                UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
-        }
+        internal static void Debug(string msg) => UnityEngine.Debug.Log($"{Constants.LogPrefixBase} {msg}");
+        internal static void Warn(string msg) => UnityEngine.Debug.LogWarning($"{Constants.LogPrefixBase} {msg}");
+        internal static void Error(string msg) => UnityEngine.Debug.LogError($"{Constants.LogPrefixBase} {msg}");
     }
 }
